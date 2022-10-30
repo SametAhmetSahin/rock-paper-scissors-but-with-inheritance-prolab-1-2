0f6d6ed2c933125b436edb287534506cf8fd4b25
NusretYILDIZ <nusretyildiz2003@gmail.com>
2022-10-30 22:31:30 +0300

    Commented out a code that initializes JFrame components.

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -2,8 +2,8 @@ import javax.swing.*;
 
 public class MainMenu extends JFrame
 {
-    private JButton dertButton = new JButton("Derdini sikeyim butonu");
-    private JPanel panel1 = new JPanel();
+    private JButton dertButton; // = new JButton("Derdini sikeyim butonu");
+    private JPanel panel1; // = new JPanel();
 
     public MainMenu(String appTitle)
     {
