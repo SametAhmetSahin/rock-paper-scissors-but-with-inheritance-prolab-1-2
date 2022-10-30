a844c7d9b6cbb04b5103feabb7f7ee58f5f69c4d
NusretYILDIZ <nusretyildiz2003@gmail.com>
2022-10-30 23:20:12 +0300

    Added functionality to 'dertButton' button.

--- src/Menu/MainMenu.java
+++ src/Menu/MainMenu.java
@@ -1,11 +1,14 @@
 package Menu;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MainMenu extends JFrame
 {
     private JButton dertButton; // = new JButton("Derdini sikeyim butonu");
     private JPanel panel1; // = new JPanel();
+    private JLabel dertText;
 
     public MainMenu(String appTitle)
     {
@@ -16,5 +19,11 @@ public class MainMenu extends JFrame
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setContentPane(panel1);
         this.pack();
+        dertButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dertText.setText("DERDİNİ SİKEYİM!!!");
+            }
+        });
     }
 }
