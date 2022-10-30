74ebca90b414a55a56419d6d0abef56a597d228f
NusretYILDIZ <nusretyildiz2003@gmail.com>
2022-10-30 22:27:37 +0300

    Added button to Swing window.

---
+++ src/MainMenu.java
@@ -0,0 +1,24 @@
+import javax.swing.*;
+
+public class MainMenu extends JFrame
+{
+    private JButton dertButton = new JButton("Derdini sikeyim butonu");
+    private JPanel panel1 = new JPanel();
+
+    public MainMenu(String appTitle)
+    {
+        super(appTitle);
+
+        dertButton.setText("Derdini sikeyim butonu");
+
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setContentPane(panel1);
+        this.pack();
+    }
+
+    public static void main(String[] args)
+    {
+        JFrame frame = new MainMenu("Anan");
+        frame.setVisible(true);
+    }
+}
