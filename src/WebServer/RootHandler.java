59a032ae949865c1dffe5c62e31b6ced44ac235a
Samet Ahmet Şahin <sametahmetsahin@protonmail.com>
2022-11-09 21:28:35 +0300

    Added actual missing content in WebServer and RootHandler, partially implemented GetHandler

--- src/WebServer/RootHandler.java
+++ src/WebServer/RootHandler.java
@@ -1,4 +1,20 @@
 package WebServer;
 
-public class RootHandler {
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class RootHandler implements HttpHandler {
+    @Override
+
+    public void handle(HttpExchange he) throws IOException {
+        String response = "<h1>Server start success " +
+        "if you see this message </h1 > " + " Merhaba Nusret ";
+        he.sendResponseHeaders(200, response.length());
+        OutputStream os = he.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
+    }
 }
--- src/WebServer/WebServer.java
+++ src/WebServer/WebServer.java
@@ -1,2 +1,29 @@
-package WebServer;public class WebServer {
+package WebServer;
+
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class WebServer {
+    public static void main(String[] args) throws IOException {
+
+        int port = 8080;
+        HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
+        System.out.println("server started at " + port);
+        server.createContext("/", new RootHandler());
+        server.createContext("/get", new GetHandler());
+        //server.createContext("/post", new PostHandler());
+        /*
+        server.createContext("/echoHeader", new EchoHeaderHandler());
+
+
+        server.createContext("/echoPost", new EchoPostHandler());
+         */
+        server.setExecutor(null);
+        server.start();
+    }
+
+
 }
+
